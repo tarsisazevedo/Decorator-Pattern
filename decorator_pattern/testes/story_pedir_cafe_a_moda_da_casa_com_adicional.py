b51ad8320a001567283f94f1e4b40b0424537c99
Tarsis Azevedo <tarsis.azevedo@gmail.com>
2009-11-17 10:36:31 -0200

    Alterado nome do arquivo de teste para melhor legibilidade.

---
+++ decorator_pattern/testes/story_pedir_cafe_a_moda_da_casa_com_adicional.py
@@ -0,0 +1,71 @@
+#encoding: utf-8
+from pyhistorian import *
+from should_dsl import *
+
+from decorator_pattern.cliente import *
+
+from decorator_pattern.bebidas.cafe_a_moda_da_casa import *
+from decorator_pattern.decorators.decorator_whip import *
+from decorator_pattern.decorators.decorator_leite import *
+from decorator_pattern.decorators.decorator_creme import *
+
+class PedindoUmCafeComAdicional(Historia):
+    """
+    Como um cliente da cafeteria
+    Eu quero comprar um cafe com adicional
+    Para que eu possa incrementar meu cafe
+    """
+    cenarios=['PedindoUmCafeAModaDaCasaComWhip', 'PedindoUmCafeAModaDaCasaComLeite', 'PedindoUmCafeAModaDaCasaComCreme']
+    colored = True
+
+class PedindoUmCafeAModaDaCasaComWhip(Cenario):
+    @DadoQue("Eu sou um cliente")
+    def eu_sou_um_cliente(self):
+        self.cliente = Cliente()
+
+    @DadoQue("e eu quero comprar um cafe a moda da casa")
+    def cafe_a_moda_da_casa(self):
+        self.cafe_a_moda_da_casa = CafeAModaDaCasa(0.99)
+
+    @DadoQue("eu quero acrescentar whip no cafe")
+    def cafe_com_whip(self):
+        self.cafe_com_whip = Whip(self.cafe_a_moda_da_casa)
+
+    @Quando("eu peço meu cafe, que custa 0.99, com whip, que custa 0.20")
+    def pedir_cafe_com_whip(self):
+        self.cliente.pedir_cafe(self.cafe_com_whip)
+
+    @Entao("eu tenho meu cafe com whip e uma conta de 1.19")
+    def eu_tenho_uma_conta(self):
+        self.cliente.get_conta() |should_be.equal_to| 1.19
+
+class PedindoUmCafeAModaDaCasaComLeite(Cenario):
+    DadoQue("Eu sou um cliente")
+    DadoQue("e eu quero comprar um cafe a moda da casa")
+    @DadoQue("eu quero acrescentar leite no meu cafe")
+    def cafe_com_leite(self):
+        self.cafe_com_leite = Leite(self.cafe_a_moda_da_casa)
+    @Quando("eu peço meu cafe, que custa 0.99, com leite, que custa 0.50")
+    def pedir_cafe_com_leite(self):
+        self.cliente.pedir_cafe(self.cafe_com_leite)
+
+    @Entao("eu tenho meu cafe com leite e uma conta de 1.49")
+    def eu_tenho_uma_conta(self):
+        self.cliente.get_conta() |should_be.equal_to| 1.49
+
+class PedindoUmCafeAModaDaCasaComCreme(Cenario):
+    DadoQue("Eu sou um cliente")
+    DadoQue("e eu quero comprar um cafe a moda da casa")
+    @DadoQue("eu quero acrescentar creme no meu cafe")
+    def cafe_com_creme(self):
+        self.cafe_com_creme = Creme(self.cafe_a_moda_da_casa)
+    
+    @Quando("eu peço meu café, que custa 0.99, com creme, que custa 0.20")
+    def pedir_cafe_com_creme(self):
+        self.cliente.pedir_cafe(self.cafe_com_creme)
+
+    @Entao("eu tenho meu cafe com creme e uma conta de 1.19")
+    def eu_tenho_uma_conta(self):
+        self.cliente.get_conta() |should_be.equal_to| 1.19    
+ 
+PedindoUmCafeComAdicional.run()
